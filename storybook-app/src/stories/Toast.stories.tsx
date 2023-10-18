922bdcbe5bd630ed494a51b262afb0543cec92c2
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-19 03:49:44 +0900

    :sparkles: Feat: 토스트 스토리 리코일 추가

--- storybook-app/src/stories/Toast.stories.tsx
+++ storybook-app/src/stories/Toast.stories.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { RecoilRoot, useRecoilState } from "recoil";
+import { RecoilRoot, useRecoilState, useSetRecoilState } from "recoil";
 import Toast from "./Toast";
 
 import { toastState } from "./Toast";
@@ -11,7 +11,11 @@ const meta = {
   component: Toast,
   decorators: [
     // story에 임의의 wrapper를 제공
-    (story: any) => <div style={{ padding: "3rem" }}>{story()}</div>,
+    (story: any) => (
+      <RecoilRoot>
+        <div style={{ padding: "3rem" }}>{story()}</div>
+      </RecoilRoot>
+    ),
   ],
 };
 export default meta;
@@ -23,3 +27,12 @@ Default.args = {
   messages: [{ message: "안녕하세요" }, { message: "알림입니다" }],
   handleSubmit: action("handleSubmit"),
 };
+
+const MessageBox = () => {
+  const [messages, setMessages] = useRecoilState(toastState);
+  setMessages((prev) => [...prev, { message: "hi" }]);
+  return <Toast messages={messages} handleSubmit={() => {}} />;
+};
+
+const MessageTemplate = () => <MessageBox />;
+export const DefaultMessageBox = MessageTemplate.bind({});
